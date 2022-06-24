8bc6400d5253dd01f74e0b1fe7c01b7e7568473a
Philipp Schlesinger <info@philcomm.dev>
2022-06-24 20:57:41 +0200

    Add Intro song action

--- code.py
+++ code.py
@@ -24,6 +24,10 @@ key4.inactive()
 key5 = KeypadKey(keypad.keys[5], (Keycode.LEFT_CONTROL, Keycode.KEYPAD_FOUR), (2, 149, 247))
 key5.inactive()
 
+key8 = KeypadKey(keypad.keys[8], (Keycode.LEFT_CONTROL, Keycode.KEYPAD_SEVEN), (240, 5, 228))
+
+active_scene = 0
+
 streaming = False
 
 while True:
@@ -56,15 +60,27 @@ while True:
 
   if key4.is_pressed():
     key5.inactive()
+    key8.active()
 
     key4.press(keyboard)
     key4.blink_for(2)
     key4.active()
+
+    active_scene = 1
   elif key5.is_pressed():
     key4.inactive()
+    key8.off()
 
     key5.press(keyboard)
     key5.blink_for(2)
     key5.active()
+
+    active_scene = 2
+  elif active_scene == 1 and key8.is_pressed():
+    key8.press(keyboard)
+    key8.inactive()
+    time.sleep(0.5)
+    key8.blink_for(2)
+    key8.active()
 
   time.sleep(0.01)
